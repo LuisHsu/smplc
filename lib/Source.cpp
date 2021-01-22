dc08f3ae5da245d02709719ae02252d44ec236a1
LuisHsu <luishsuplus@gmail.com>
2021-01-21 19:01:39 -0800

    Insert license header

--- lib/Source.cpp
+++ lib/Source.cpp
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2021 Luis Hsu. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 #include <Source.hpp>
 
 #include <Exception.hpp>
